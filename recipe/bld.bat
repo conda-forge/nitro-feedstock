6d5aa113f7076c5ce7987a65f472b6b3f1e30a16
Howard Butler <howard@hobu.co>
2018-10-25 19:44:30 -0500

    support NITRO_PLUGIN_PATH on windows for alternative TRE implementations. Update to 2.7dev-3. (#3)
    * support NITRO_PLUGIN_PATH on windows for alternative TRE implementations
    
    * bump to 2.7dev-3 for exeception specification issues
    
    * remove NITF_PLUGIN_PATH for now
    
    * build no 0 for new version dev3
    
    * MNT: Re-rendered with conda-smithy 3.1.12 and pinning 2018.10.16

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,11 +4,15 @@ cd build
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
+	  -DBUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake
 if errorlevel 1 exit 1
 
+
 nmake install
 if errorlevel 1 exit 1
+
+
