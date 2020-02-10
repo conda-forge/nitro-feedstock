9785bef9dd1b3794c58dce53589884236f58a37f
Howard Butler <howard@hobu.co>
2020-02-10 12:25:51 -0600

    try to get auto_ptr for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" ^
+CXXFLAGS="/D_HAS_AUTO_PTR_ETC=1" cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
 	  -DBUILD_SHARED_LIBS=ON ^
